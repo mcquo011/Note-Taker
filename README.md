0ada1c37861546f590a24b7f576d839121378959
mcquo011 <omcquown@gmail.com>
2023-02-22 12:38:22 -0600

    post request working

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Note-Taker
+# Note Taker
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+ ## Table of Contents 📑
+
+  * [Description](#description)
+  * [Dependencies](#dependencies)
+  * [Usage](#usage)
+  * [Contributers](#contributers)
+  * [Test](#tests)
+  * [License](#license)
+  * [Questions](#questions)
+
+  ## Description 
+
+
+  ## Dependencies  
+
+  npm i inquirer@8.2.4 and npm i uuid
+
+  ## Usage 
+
+
+
+  ## Contributers 
+
+  Maggie McQuown
+
+  ## Tests 
+
+
+  ## License 
+  
+  Copyright (c)
+  [MIT](https://opensource.org/licenses/MIT)
+
+  ## Questions 
+
+  You can visit my Github Profile here: [mcquo011](https://github.com/mcquo011/) 
+  If you have any questions please email me at omcquown@gmail.com
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static("Develop/public"));
 
 app.get("/api/notes", (req, res) => {
   fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
@@ -24,9 +24,34 @@ app.get("/api/notes", (req, res) => {
 
 
 // receive a new note to sav on the request body, add it to the db.json, and then return the new note to the client
+
 app.post('/api/notes', (req, res) => {
- 
-})
+  // generate a unique ID for the new note
+  const newNoteId = uuid.v4();
+  // create a new note object with the provided request body and generated ID
+  const newNote = {
+    id: newNoteId,
+    title: req.body.title,
+    text: req.body.text
+  };
+  // read the current notes from db.json
+  const notes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
+  // add the new note to the array of notes
+  notes.push(newNote);
+  // write the updated notes back to db.json
+  fs.writeFileSync("./Develop/db/db.json", JSON.stringify(notes));
+  // return the new note as the response
+  res.json(newNote);
+});
+
+// get call for notes.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
